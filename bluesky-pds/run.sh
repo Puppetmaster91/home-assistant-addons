1d5e87ea29145e7ddf8534b9a8df635ce324f63a
Puppetmaster91 <63652895+Puppetmaster91@users.noreply.github.com>
2025-06-25 09:39:09 +0200

    Update run.sh

--- bluesky-pds/run.sh
+++ bluesky-pds/run.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+set -e
+
+echo "Starting Bluesky PDS..."
+
+# Print variables
+echo "HANDLE: $HANDLE"
+echo "PDS_HOSTNAME: $PDS_HOSTNAME"
+echo "DB_URL: $DB_URL"
+
+# Export them for node
 export PDS_HANDLE="${HANDLE}"
 export PDS_HOSTNAME="${PDS_HOSTNAME}"
 export PDS_DB_URL="${DB_URL}"
 export PDS_REPO_SIGNING_KEY="${REPO_SIGNING_KEY}"
 export PDS_ADMIN_PASSWORD="${ADMIN_PASSWORD}"
 
-echo "Launching Bluesky PDS with the following settings:"
-echo "Handle: $PDS_HANDLE"
-echo "Hostname: $PDS_HOSTNAME"
-echo "DB: $PDS_DB_URL"
+# Check node is installed
+which node || { echo "Node.js is not installed!"; exit 1; }
 
-# Start the PDS
+# Run the app
 exec node ./bin.js
